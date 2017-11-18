d18f75f0cf7659897302ce2fec9318fbff95fe09
Harry Martland <harryemartland@gmail.com>
2017-11-18 11:32:42 +0000

    Remove docker install and update

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-apt-get update
-apt-get install -y wget sudo
-
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
