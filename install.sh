44afa288134076e88ef3f5e884170d0c9d07eb8e
Harry Martland <harryemartland@gmail.com>
2017-11-18 10:45:33 +0000

    Added wget install

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+sudo apt-get install wget
+
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
