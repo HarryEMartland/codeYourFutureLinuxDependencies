a423e1b774662e8dbe7c035d30be2f9dc768b5a5
Harry Martland <harryemartland@gmail.com>
2018-01-20 14:52:43 +0000

    Update install.sh
    upgraded node

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
     ((i=i+1))
 done 
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
